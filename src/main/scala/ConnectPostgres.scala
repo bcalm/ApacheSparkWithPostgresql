401d4376f3f1c82741e8b0972d4b097652bfe06c
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2021-05-24 08:28:02 +0530

    Calculate monthly revenue for each customer

--- src/main/scala/ConnectPostgres.scala
+++ src/main/scala/ConnectPostgres.scala
@@ -1,21 +1,22 @@
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
-
 import java.util.Properties
 
 object ConnectPostgres {
 
   def main(args: Array[String]): Unit = {
 
+
     val spark = SparkSession
       .builder()
       .appName("connect postgresql")
-      .master("local")
+      .master("local[3]")
       .getOrCreate()
 
-    val jdbcDF = spark.read
+    val orders = spark.read
       .format("jdbc")
-      .option("url", "jdbc:postgresql:apachedemo")
-      .option("dbtable", "persons")
+      .option("url", "jdbc:postgresql:orders")
+      .option("dbtable", "orders")
       .option("user", "vikram")
       .option("password", "mypassword")
       .load()
@@ -23,14 +24,36 @@ object ConnectPostgres {
     val connectionProperties = new Properties()
     connectionProperties.put("user", "vikram")
     connectionProperties.put("password", "mypassword")
-    val jdbcDF2 = spark.read
-      .jdbc("jdbc:postgresql:apachedemo", "persons", connectionProperties)
+    val ordersItems = spark.read
+      .jdbc("jdbc:postgresql:orderitem", "orderitems", connectionProperties)
 
-    jdbcDF2.show()
+    val customers = spark.read
+      .jdbc("jdbc:postgresql:customers", "customers", connectionProperties)
 
-    val jdbcDF3 = spark.read
-      .jdbc("jdbc:postgresql:apachedemo", "persons", connectionProperties)
+    val filteredOrders = orders.filter("order_status IN ('COMPLETE', 'CLOSED', 'PENDING')")
 
-    jdbcDF3.show()
+
+    val joinResult = filteredOrders
+      .join(ordersItems, ordersItems("order_item_order_id") === orders("order_id"))
+      .join(customers, customers("customer_id") === orders("order_customer_id"))
+      .select("customer_fname", "customer_lname", "order_date", "order_item_subtotal")
+
+
+    import spark.implicits._
+    val concattedCustomerName = joinResult
+      .select(
+        concat($"customer_fname", lit(" "), $"customer_lname").as("name"),
+        date_format($"order_date", "yyyyMM").as("month"),
+        $"order_item_subtotal"
+      )
+
+    val result = concattedCustomerName.groupBy("month", "name")
+      .agg(round(sum("order_item_subtotal"), 2).as("revenue"))
+      .orderBy(asc("month"), desc("revenue"))
+
+
+    result.write
+      .jdbc("jdbc:postgresql:apachedemo", "monthlyExpense", connectionProperties)
+
   }
 }
